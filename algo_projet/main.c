471febe60b9b45a5160cb121f08b21b8d0648b49
FedsiImane <iman14092003@gmail.com>
2024-01-09 18:55:58 +0100

    creation node

--- algo_projet/main.c
+++ algo_projet/main.c
@@ -1,9 +1,33 @@
 
 
 #include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+ 
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
 
+void createNode(Node** head, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode != NULL) {
+        newNode->data = data;
+        newNode->next = *head;
+        *head = newNode;
+    }
+}
 
-int main()
-{
+int main(){
+    Node* myList = NULL;
+    int i , val , n;
+    
+    printf (" entrer n :");
+    scanf ("%d",&n);
+    for (i=0 ; i< n ; i++){
+        printf (" entrer val :");
+        scanf ("%d",&val);
+        createNode(&myList, val);
+    }
    
 }

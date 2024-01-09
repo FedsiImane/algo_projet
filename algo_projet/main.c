3679c752d9fb69b97fbcdd35997b860f29493487
FedsiImane <iman14092003@gmail.com>
2024-01-09 19:46:19 +0100

    changes

--- algo_projet/main.c
+++ algo_projet/main.c
@@ -42,42 +42,7 @@ void freeList(Node* head) {
         current = next;
     }
 }
-void selectionSortAscending(Node** head) {
-    Node *i, *j, *minNode;
-    int frameCount = 0;
 
-    for (i = *head; i != NULL; i = i->next) {
-        minNode = i;
-
-        for (j = i->next; j != NULL; j = j->next) {
-            if (j->data < minNode->data) {
-                minNode = j;
-            }
-        }
-
-        int temp = i->data;
-        i->data = minNode->data;
-        minNode->data = temp;
-    }
-}
-void selectionSortDescending(Node** head) {
-    Node *i, *j, *maxNode;
-    int frameCount = 0;
-
-    for (i = *head; i != NULL; i = i->next) {
-        maxNode = i;
-
-        for (j = i->next; j != NULL; j = j->next) {
-            if (j->data > maxNode->data) {
-                maxNode = j;
-            }
-        }
-
-        int temp = i->data;
-        i->data = maxNode->data;
-        maxNode->data = temp;
-    }
-}
 void drawListWithAnimation(Node* head, Node* node1, Node* node2, const char* swapDetails, int frameCount) {
     const int spacing = 60;
     int posX = 50;
@@ -102,6 +67,63 @@ void drawListWithAnimation(Node* head, Node* node1, Node* node2, const char* swa
     }
 
     DrawText(TextFormat("Frame: %d", frameCount), 10, 430, 20, BLACK);
+}
+
+void selectionSortAscending(Node** head) {
+    Node *i, *j, *minNode;
+    int frameCount = 0;
+
+    for (i = *head; i != NULL; i = i->next) {
+        minNode = i;
+
+        for (j = i->next; j != NULL; j = j->next) {
+            if (j->data < minNode->data) {
+                minNode = j;
+            }
+        }
+
+        int temp = i->data;
+        i->data = minNode->data;
+        minNode->data = temp;
+        
+        char swapDetails[50];
+        sprintf(swapDetails, "Swapped %d with %d", i->data, minNode->data);
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Selection Sort (Ascending)", 10, 10, 20, BLACK);
+        drawListWithAnimation(*head, i, minNode, swapDetails, frameCount++);
+        EndDrawing();
+        for (int delay = 0; delay < 1000000000; delay++);
+    }
+}
+void selectionSortDescending(Node** head) {
+    Node *i, *j, *maxNode;
+    int frameCount = 0;
+
+    for (i = *head; i != NULL; i = i->next) {
+        maxNode = i;
+
+        for (j = i->next; j != NULL; j = j->next) {
+            if (j->data > maxNode->data) {
+                maxNode = j;
+            }
+        }
+
+        int temp = i->data;
+        i->data = maxNode->data;
+        maxNode->data = temp;
+        
+        char swapDetails[50];
+        sprintf(swapDetails, "Swapped %d with %d", i->data, maxNode->data);
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Selection Sort (Descending)", 10, 10, 20, BLACK);
+        drawListWithAnimation(*head, i, maxNode, swapDetails, frameCount++);
+        EndDrawing();
+        for (int delay = 0; delay < 1000000000; delay++);
+    }
 }
 
         
